ab0fa5c4510415c1eda72eafa1589905d1431ed7
TechDevMark <ariski@smklabor.sch.id>
2026-09-24 18:45:06 +0700

    config: enable trustProxies for reverse proxy and https support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
