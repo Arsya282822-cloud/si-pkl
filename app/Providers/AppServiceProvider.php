59054da4248056a3f9f504865e9030475565e34f
TechDevMark <ariski@smklabor.sch.id>
2026-09-24 18:51:22 +0700

    fix: enforce https scheme to resolve mixed content insecure form submission warning

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        // Enforce HTTPS Scheme jika diakses via HTTPS, Reverse Proxy, Cloudflare, Ngrok, atau Server Production
+        if (
+            request()->server('HTTP_X_FORWARDED_PROTO') === 'https' ||
+            request()->header('X-Forwarded-Proto') === 'https' ||
+            request()->isSecure() ||
+            str_contains(config('app.url'), 'https://') ||
+            $this->app->environment('production')
+        ) {
+            URL::forceScheme('https');
+        }
 
         try {
             if (!\Illuminate\Support\Facades\Schema::hasTable('lembar_observasi')) {
